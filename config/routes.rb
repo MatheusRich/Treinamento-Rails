8f06e1e59280beab157f1e1cfb04dfaf0bca83f6
MatheusRich <matheusrichardt@gmail.com>
2017-08-31 10:38:55 -0300

    Adding new method and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
   devise_for :users
+  resources :posts
+  root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
